353d4b6bc6493caa572b0833c7029064c183b446
pkowalicki <przemekkowalicki@gmail.com>
2021-02-19 09:33:49 +0100

    Update config.ts
    ok

--- capstone/client/src/config.ts
+++ capstone/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = 'https://9k5qx30o4h.execute-api.eu-central-1.amazonaws.com/dev'
+export const apiEndpoint = '...'
 
 export const authConfig = {
-  domain: 'dev-46nx7vs6.eu.auth0.com',
-  clientId: '63v8KzS448oFXJUuqmMkfA35mTX71fU7',
+  domain: '...',
+  clientId: '...',
   callbackUrl: 'http://localhost:3000/callback'
 }
